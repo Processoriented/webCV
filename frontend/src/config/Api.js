bf902fc11c9d00f9893f93657cd9de0c72b35386
Vincent Engler <vincent.engler@ge.com>
2017-09-18 21:25:16 -0500

    updated baseline with OAuth details

--- frontend/src/config/Api.js
+++ frontend/src/config/Api.js
@@ -1,2 +1,4 @@
 export const URL = process.env.API_URL
-export const LOGIN = '/auth'
+export const LOGIN = '/o'
+export const CLIENT_ID = 'YligNZTAGXY6nJ3717tPqFuVf5yb68ymHHqNqJt9'
+export const CLIENT_SECRET = 'CUzoq4K0Dlg9w9V057vKr3mf8x6fUBtGcssRSdg65NfuPaFHRoOwwOoJTjV9bf9p4yUbeVTvkZ7gWsIJ1WymwazS7Oi2hn3MRcS86jKzfqW9wmzd0DgUSvisNEIoj532'
--- frontend/src/util/Auth.js
+++ frontend/src/util/Auth.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import _ from 'lodash'
 import store from '../store'
 import { setToken } from '../actions'
-import { URL, LOGIN } from '../config/Api'
+import { URL, LOGIN, CLIENT_ID, CLIENT_SECRET } from '../config/Api'
 
 export function InvalidCredentialsException(message) {
   this.message = message
@@ -11,7 +11,7 @@ export function InvalidCredentialsException(message) {
 
 export const login = (username, password) => (
   axios
-    .post(URL + LOGIN, { username, password })
+    .post(URL + LOGIN, { username, password, client_id: CLIENT_ID, client_secret: CLIENT_SECRET })
     .then(response => store.dispatch(setToken(response.data.token)))
     .catch((error) => {
       // raise different exception if due to invalid credentials
